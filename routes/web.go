67198865d719460bfecce84aa18038d7f572ecba
hidochunk <jack8426jack@gmail.com>
2022-10-16 10:33:05 +0800

    change html file path & delete old path

--- routes/web.go
+++ routes/web.go
@@ -1,1 +1,13 @@
 package routes
+
+import "github.com/gin-gonic/gin"
+
+func WebRouter() *gin.Engine {
+	router := gin.New()
+
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{})
+	})
+
+	return router
+}
